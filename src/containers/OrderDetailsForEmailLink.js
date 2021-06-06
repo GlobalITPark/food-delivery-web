191c3877fe8d69dc9fd81d0f95355afae434ebfd
Geethananth <geethananth@neovibe.in>
2021-06-06 16:11:13 +0530

    Order details direct page added

--- src/containers/OrderDetailsForEmailLink.js
+++ src/containers/OrderDetailsForEmailLink.js
@@ -58,35 +58,35 @@ class OrderDetailsForEmailLink extends Component {
             <div className="col-md-6">
                 
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('orderNo')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.id}</strong>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('Delivery/ Pickup Time')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.orderDateTime}</strong>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('Name of the customer')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.delivery.name}</strong>
                     </div>
                 </div>
                 
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('Phone Number')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.delivery.phone}</strong>
                     </div>
                 </div>           
@@ -95,26 +95,26 @@ class OrderDetailsForEmailLink extends Component {
         <div className ="col-md-6">
         
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('Order type')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.deliveryType}</strong>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('Delivery Instructions')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.deliveryInstructions}</strong>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-5">
                         <h6 className="mb-3"> {translate('Address')} :</h6>
                     </div>
-                    <div className="col-md-8" style={{paddingTop: '26px'}}>
+                    <div className="col-md-7" style={{paddingTop: '26px'}}>
                         <strong>{this.state.order.deliveryAddress}</strong>
                     </div>
                 </div>
